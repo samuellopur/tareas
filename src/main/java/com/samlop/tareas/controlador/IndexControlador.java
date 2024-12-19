f7923a3a68a3bd987c0f5e9146ffeaf551a83e37
Samuel Lopez <samuellopez01@hotmail.com>
2024-12-19 07:43:48 -0500

    Integracion JavaFX con Spring

--- src/main/java/com/samlop/tareas/controlador/IndexControlador.java
+++ src/main/java/com/samlop/tareas/controlador/IndexControlador.java
@@ -1,13 +1,15 @@
 package com.samlop.tareas.controlador;
 
 import javafx.fxml.Initializable;
+import org.springframework.stereotype.Component;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+@Component
 public class IndexControlador implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        
+
     }
 }
