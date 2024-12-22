f1883b7fdc74996c2eea926b0f0cb738438f2225
Samuel Lopez <samuellopez01@hotmail.com>
2024-12-22 13:04:53 -0500

    Limpiar formulario y eliminar tarea

--- src/main/java/com/samlop/tareas/controlador/IndexControlador.java
+++ src/main/java/com/samlop/tareas/controlador/IndexControlador.java
@@ -128,7 +128,21 @@ public class IndexControlador implements Initializable {
         listarTareas();
     }
 
-    private void limpiarFormulario(){
+    public void eliminarTarea(){
+        var tarea = tareaTabla.getSelectionModel().getSelectedItem();
+        if (tarea != null){
+            logger.info("Registro a eliminar: " + tarea.toString());
+            tareaServicio.eliminarTarea(tarea);
+            mostrarMensaje("Información", "Tarea eliminada: " + tarea.getIdTarea());
+            limpiarFormulario();
+            listarTareas();
+        }
+        else {
+            mostrarMensaje("Error", "No se ha seleccionado ninguna tarea");
+        }
+    }
+
+    public void limpiarFormulario(){
         idTareaInterno = null;
         nombreTareaTexto.clear();
         responsableTexto.clear();
